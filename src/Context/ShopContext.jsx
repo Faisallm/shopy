357af8501aed82f6f2a0c792ad43ee502890aadc
Faisallm <faisallawan1997@gmail.com>
2023-10-12 15:52:29 -0700

    Added Context, global state

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import all_product from '../Components/Assets/all_product';
+
+// is this like redux?
+// context is used to manage state globally. It can be used...
+// together with the useState hook to share state between
+// deeply nested components more easily.
+
+// is it like a global state
+// making all_products available to the entire components
+// app wide
+
+export const ShopContext = createContext(null);
+
+const ShopContextProvider = (props) => {
+
+    // this is the data that will be available...
+    // application wide
+    const contextValue = {all_product}
+
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
+
+
+// this code file is basically boiler plate.
+// the only important information here is the contextValue
