c86fa27214660b68f051d77f823dc99a80e8437b
Faisallm <faisallawan1997@gmail.com>
2023-10-12 17:09:21 -0700

    Done with login page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from "./Pages/LoginSignup";
 import Footer from "./Components/Footer/Footer";
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kids_banner from './Components/Assets/banner_kids.png';
 
 function App() {
   return (
@@ -24,9 +27,9 @@ function App() {
 
           <Route path="/" element={<Shop/>}/>
           {/* adding props */}
-          <Route path="/mens" element={<ShopCategory category='men'/>}/>
-          <Route path="/womens" element={<ShopCategory category='women'/>}/>
-          <Route path="/kids" element={<ShopCategory category='kid'/>}/>
+          <Route path="/mens" element={<ShopCategory category='men' banner={men_banner}/>}/>
+          <Route path="/womens" element={<ShopCategory category='women' banner={women_banner}/>}/>
+          <Route path="/kids" element={<ShopCategory category='kid' banner={kids_banner}/>}/>
           {/* so is this like saying we can have nested routing */}
           <Route path="/product" element={<Product/>}>
             <Route path=":productId" element={<Product/>}/>
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useContext } from "react";
+import "./CSS/ShopCategory.css";
+import { ShopContext } from "../Context/ShopContext";
+import dropDownIcon from "../Components/Assets/dropdown_icon.png";
+import Item from "../Components/Item/Item";
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  // this is how we access the global state
+  //  we will use props to filter our product
+  const {all_product} = useContext(ShopContext);
+
   return (
-    <div>ShopCategory</div>
-  )
-}
+    <div className="shop-category">
+      <img className="shopcategory-banner" src={props.banner} alt="Banner" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropDownIcon} alt="Dropdown Icon" />
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_product.map((item, i) => {
+          if (props.category === item.category) {
+            return (
+              <Item
+                key={i}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          }
+          else {
+            return null;
+          }
+        })}
+      </div>
 
-export default ShopCategory
+      <div className="shopcategory-loadmore">
+        Explore more
+      </div>
+    </div>
+  );
+};
+
+export default ShopCategory;
